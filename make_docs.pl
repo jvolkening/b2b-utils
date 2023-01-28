2e292c7dd5ae04d9a7ec89c03577e5e8ff53b248
jvolkening <jdv@base2bio.com>
2023-01-28 11:44:43 -0600

    build docs for Pages

---
+++ make_docs.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+package Pod::Simple::Search::NoRecurse;
+
+use parent Pod::Simple::Search;
+
+sub new {
+
+    my $self = Pod::Simple::Search->new;
+    $self->recurse(0);
+    $self->laborious(1);
+    return $self;
+
+}
+
+package main;
+
+use strict;
+use warnings;
+use 5.012;
+
+use Pod::Simple::HTMLBatch;
+use Pod::Simple::Search;
+
+# read in header/footer
+open my $in, '<', 'doc/templates/header.html';
+my $header = do { local $/; <$in> };
+close $in;
+open $in, '<', 'doc/templates/footer.html';
+my $footer = do { local $/; <$in> };
+close $in;
+
+my $conv = Pod::Simple::HTMLBatch->new;
+$conv->search_class('Pod::Simple::Search::NoRecurse');
+$conv->add_css('style.css');
+$conv->css_flurry(0);
+$conv->javascript_flurry(0);
+$conv->contents_page_start($header);
+$conv->contents_page_end($footer);
+
+$conv->batch_convert(
+    $ARGV[0],
+    $ARGV[1],
+);
